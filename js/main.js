afd32f00487bda4c0d4154d2bfd83ccc20961c38
x404 <kronos2003@gmail.com>
2021-04-21 18:17:09 +0300

    incapsulation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
-//(function () {
+// create object and grid
 let objOfCells = {};
+const CELLCOUNTS = 100;
 const fragment = document.createDocumentFragment();
 const list = document.querySelector(".list");
-const TIMEOUT = 1200; //ms
-const FINISHCOUNT = 10;
-const CELLCOUNTS = 100;
-let countSuccess = 0;
-let countError = 0;
-const humanCountEl = document.querySelector("#human_count");
-const computerCountEl = document.querySelector("#computer_count");
 
-let flag = false;
-
-// create object and grid
 for (let i = 1; i <= CELLCOUNTS; i++) {
   createObj(i);
   renderGrid(i);
@@ -36,96 +27,106 @@ function renderGrid(id) {
 
 list.appendChild(fragment);
 
-// =BLINK CELLS
-let timer;
-let prevId;
+(function () {
+  const TIMEOUT = 1200; //ms
+  const FINISHCOUNT = 10;
+  let countSuccess = 0;
+  let countError = 0;
+  const humanCountEl = document.querySelector("#human_count");
+  const computerCountEl = document.querySelector("#computer_count");
 
-document.querySelector(".start").addEventListener("click", function () {
-  // starting position
-  const arr = Object.entries(objOfCells);
-  const rnd = randomInteger(arr.length - 1, "start");
-  const _id = arr[rnd][1]._id;
+  let flag = false;
 
-  // set active cell
-  document.querySelector(`[data-id="${_id}"]`).classList.add("cell-active");
+  // =BLINK CELLS
+  let timer;
+  let prevId;
 
-  // start timer
-  prevId = _id;
-  timer = setInterval(blinkCell, TIMEOUT);
-});
+  document.querySelector(".start").addEventListener("click", function () {
+    // starting position
+    const arr = Object.entries(objOfCells);
+    const rnd = randomInteger(arr.length - 1, "start");
+    const _id = arr[rnd][1]._id;
 
-let out = "";
-function blinkCell() {
-  //  if cell was active and no pressed it
-  if (!flag) {
-    document.querySelector(".cell-active").classList.add("cell-error");
-    document.querySelector(".cell-active").classList.remove("cell-active");
-
-    updateStatusCellInObj(prevId, "error");
-  }
-
-  const arr = Object.entries(objOfCells).filter(
-    (el) => el[1].error == false && el[1].success == false
-  );
-
-  if (arr.length === 0 || !checkResult()) {
-    clearInterval(timer);
-    return;
-  }
-
-  // select random element from array
-  const rnd = randomInteger(arr.length - 1, " intimer");
-  const _id = arr[rnd][1]._id;
-
-  prevId = _id;
-  if (countError < FINISHCOUNT && countSuccess < FINISHCOUNT) {
-    // set active next cell in html
+    // set active cell
     document.querySelector(`[data-id="${_id}"]`).classList.add("cell-active");
 
-    // console.log("arr[" + rnd + "][1]." + _id + "=", _id);
-    // console.log("");
+    // start timer
+    prevId = _id;
+    timer = setInterval(blinkCell, TIMEOUT);
+  });
+
+  let out = "";
+  function blinkCell() {
+    //  if cell was active and no pressed it
+    if (!flag) {
+      document.querySelector(".cell-active").classList.add("cell-error");
+      document.querySelector(".cell-active").classList.remove("cell-active");
+
+      updateStatusCellInObj(prevId, "error");
+    }
+
+    const arr = Object.entries(objOfCells).filter(
+      (el) => el[1].error == false && el[1].success == false
+    );
+
+    if (arr.length === 0 || !checkResult()) {
+      clearInterval(timer);
+      return;
+    }
+
+    // select random element from array
+    const rnd = randomInteger(arr.length - 1, " intimer");
+    const _id = arr[rnd][1]._id;
+
+    prevId = _id;
+    if (countError < FINISHCOUNT && countSuccess < FINISHCOUNT) {
+      // set active next cell in html
+      document.querySelector(`[data-id="${_id}"]`).classList.add("cell-active");
+
+      // console.log("arr[" + rnd + "][1]." + _id + "=", _id);
+      // console.log("");
+    }
+    flag = false;
   }
-  flag = false;
-}
 
-// random number
-function randomInteger(max, st) {
-  let rand = Math.floor(Math.random() * (max + 1));
-  return rand;
-}
-
-//click cell
-document.querySelector(".list").addEventListener("click", function (e) {
-  const target = e.target;
-  if (target.classList.contains("cell-active")) {
-    const id = target.dataset.id;
-    flag = true;
-    updateStatusCellInObj(id, "success");
-    checkResult();
-
-    target.classList.remove("cell-active");
-    target.classList.add("cell-success");
+  // random number
+  function randomInteger(max, st) {
+    let rand = Math.floor(Math.random() * (max + 1));
+    return rand;
   }
-});
 
-function updateStatusCellInObj(id, key) {
-  objOfCells[id][key] = true;
-  // console.log("objOfCells[" + id + "][" + key + "]=", objOfCells[id][key]);
-}
+  //click cell
+  document.querySelector(".list").addEventListener("click", function (e) {
+    const target = e.target;
+    if (target.classList.contains("cell-active")) {
+      const id = target.dataset.id;
+      flag = true;
+      updateStatusCellInObj(id, "success");
+      checkResult();
 
-function checkResult() {
-  countSuccess = Object.entries(objOfCells).filter((el) => el[1].success == true).length;
-  countError = Object.entries(objOfCells).filter((el) => el[1].error == true).length;
+      target.classList.remove("cell-active");
+      target.classList.add("cell-success");
+    }
+  });
 
-  humanCountEl.textContent = countSuccess;
-  computerCountEl.textContent = countError;
-
-  if (countSuccess == FINISHCOUNT || countError == FINISHCOUNT) {
-    console.log("%c- STOP GAME -", "color: red;font-weight:bold");
-    // console.log(objOfCells);
-    clearInterval(timer);
-    return false;
+  function updateStatusCellInObj(id, key) {
+    objOfCells[id][key] = true;
+    // console.log("objOfCells[" + id + "][" + key + "]=", objOfCells[id][key]);
   }
-  return true;
-}
-//})();
+
+  function checkResult() {
+    countSuccess = Object.entries(objOfCells).filter((el) => el[1].success == true).length;
+    countError = Object.entries(objOfCells).filter((el) => el[1].error == true).length;
+
+    humanCountEl.textContent = countSuccess;
+    computerCountEl.textContent = countError;
+
+    if (countSuccess == FINISHCOUNT || countError == FINISHCOUNT) {
+      console.log("%c- STOP GAME -", "color: red;font-weight:bold");
+      // console.log(objOfCells);
+      clearInterval(timer);
+      return false;
+    }
+    return true;
+  }
+})(objOfCells);
